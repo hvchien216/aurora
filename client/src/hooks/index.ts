9d31acc1591e7ba490f3726fec4c36f593f8a13d
Chien Lws <iocutien2112@gmail.com>
2025-04-12 23:26:46 +0700

    chore (FE): add upload hook
    Signed-off-by: Chien Lws <iocutien2112@gmail.com>

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./use-mobile";
 export * from "./use-toast";
 export * from "./use-debounce-value";
+export * from "./use-upload-mutation";
---
+++ client/src/hooks/use-upload-mutation.ts
@@ -0,0 +1,43 @@
+import { useMutation, type UseMutationOptions } from "@tanstack/react-query";
+
+import { customMutator } from "~/lib";
+
+interface UploadPayload {
+  file: File;
+}
+
+type Response = {
+  key: string;
+  url: string;
+};
+
+type Variables = UploadPayload;
+
+function createUploadFormData(payload: Variables): FormData {
+  const formData = new FormData();
+  formData.append("file", payload.file);
+  return formData;
+}
+
+export const useUploadMutation = <
+  TData extends Response,
+  TVariables extends Variables,
+>(
+  options?: Omit<
+    UseMutationOptions<TData, any, TVariables>,
+    "mutationKey" | "mutationFn"
+  >,
+) => {
+  return useMutation<TData, any, TVariables>({
+    ...options,
+    mutationKey: ["UPLOAD_IMAGE"],
+    mutationFn: (variables: TVariables) => {
+      const formData = createUploadFormData(variables);
+      return customMutator<TData, FormData>("post", `/v1/upload`, formData, {
+        headers: {
+          "Content-Type": undefined as any, // just a tricky to passed type checking
+        },
+      })();
+    },
+  });
+};
